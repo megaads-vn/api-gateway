8aece44dc136404b3abf1582fc8f7e0ffe8b2c70
PhuLuong <phult.contact@gmail.com>
2018-03-06 09:49:26 +0700

    use injection in service registry

--- start/service-registry.js
+++ start/service-registry.js
@@ -1,14 +1,11 @@
-
-var config = require(__dir + "/core/app/config");
-
-module.exports = function ($serviceRegistry) {
-    $serviceRegistry.register("CUSTOMER_SERVICE", config.get("app.serviceUrl"));
-    $serviceRegistry.register("ORDER_SERVICE", config.get("app.serviceUrl"));
-    $serviceRegistry.register("SEATING_SERVICE", config.get("app.serviceUrl"));
-    $serviceRegistry.register("BUSINESS_SERVICE", config.get("app.serviceUrl"));
-    $serviceRegistry.register("USER_SERVICE", config.get("app.serviceUrl"));
-    $serviceRegistry.register("CATEGORY_SERVICE", config.get("app.serviceUrl"));
-    $serviceRegistry.register("PRODUCT_SERVICE", config.get("app.serviceUrl"));
-    $serviceRegistry.register("IMAGE_SERVICE", config.get("app.serviceUrl"));
-    $serviceRegistry.register("TEST_SERVICE", config.get("app.serviceUrl"));
+module.exports = function ($serviceRegistry, $config) {
+    $serviceRegistry.register("CUSTOMER_SERVICE", $config.get("app.serviceUrl"));
+    $serviceRegistry.register("ORDER_SERVICE", $config.get("app.serviceUrl"));
+    $serviceRegistry.register("SEATING_SERVICE", $config.get("app.serviceUrl"));
+    $serviceRegistry.register("BUSINESS_SERVICE", $config.get("app.serviceUrl"));
+    $serviceRegistry.register("USER_SERVICE", $config.get("app.serviceUrl"));
+    $serviceRegistry.register("CATEGORY_SERVICE", $config.get("app.serviceUrl"));
+    $serviceRegistry.register("PRODUCT_SERVICE", $config.get("app.serviceUrl"));
+    $serviceRegistry.register("IMAGE_SERVICE", $config.get("app.serviceUrl"));
+    $serviceRegistry.register("TEST_SERVICE", $config.get("app.serviceUrl"));
 };
