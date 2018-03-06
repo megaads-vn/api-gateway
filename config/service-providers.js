75f724b9e1173a876c0c6b765a84ffc6f2c161ed
PhuLuong <phult.contact@gmail.com>
2018-03-06 09:57:29 +0700

    broadcast service message event

--- config/service-providers.js
+++ config/service-providers.js
@@ -3,10 +3,12 @@ var routerLoader = require(__dir + "/core/loader/route-loader");
 var serviceRegistry = require(__dir + "/core/loader/service-registry");
 var event = require(__dir + "/core/app/event");
 var logger = (require(__dir + "/core/log/logger-factory")).getLogger();
+var socketIOConnection = require(__dir + "/core/net/socket-io-connection");
 module.exports = function ($serviceContainer) {
     $serviceContainer.bind("$config", config);
     $serviceContainer.bind("$route", routerLoader);
     $serviceContainer.bind("$serviceRegistry", serviceRegistry);
     $serviceContainer.bind("$event", event);
     $serviceContainer.bind("$logger", logger);
+    $serviceContainer.bind("$socketIOConnection", socketIOConnection);
 };
